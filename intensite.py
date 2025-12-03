14f7077ef6b84d3e12c9126863465c7f53955512
Joanny-Magat <joanny.magat@etu.umontpellier.fr>
2025-12-03 18:15:33 +0100

    Add files via upload

---
+++ intensite.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Auteur : Joanny Magat
+
+"""
+Script qui prend en entré le fichier Spectre_photoluminescence.txt
+(contenant l'intensité de chaque longueur d'onde) et la taille de fenêtres pour les longueurs d'onde.
+Il renvoie un dictionnaire de liste dont la clée est la plages de longueurs d'onde et
+la valeur une liste contenant le nombre d'intensité par plage, le minimum d'intensité pour la plage,
+la moyenne et le maximum.
+"""
+
+#python3 intensite.py /home/e20210012399/Documents/M1/SYSTEME/PROJET/Spectre_photoluminescence.txt
+
+import sys, re
+
+def help():
+    #Initialisation des paramètres d'entrée
+
+    if len(sys.argv)==2 :
+        cheminFichier = sys.argv[1]
+        tailleFenetre = 10.0
+    elif len(sys.argv)==3 :
+        cheminFichier = sys.argv[1]
+        tailleFenetre = float(sys.argv[2])
+        if tailleFenetre <=0 :
+            print("ERREUR : la taille de fenêtre de longueur d'onde doit être > 0")
+            exit()
+    else :
+        print("ERREUR : il faut 1 ou 2 paramètres en entré du fichier : au moins le fichier .txt.")
+        print("En second paramètre, on peut aussi mettre la taille de fenêtres pour les longueurs d'onde (10 par défaut)")
+        exit()
+    return cheminFichier,tailleFenetre
+
+
+def premierDico(cheminFichier,tailleFenetre):
+    #Ouverture du fichier et séparation des lignes.
+    #Les lignes sont séparées en longueur et en intensité.
+    #Les données sont stockées dans un premier dictionnaire de clée plage de longueur
+    #et de valeur la liste des intensités dans cette plage
+
+
+    #Initialisation du dico
+    dico1 = {}
+    if tailleFenetre.is_integer():
+        plageMin=0
+        plageMax= plageMin + int(tailleFenetre)
+    else :
+        plageMin = 0.0 #début de la plage en cours 
+        plageMax = plageMin + tailleFenetre
+    plage = f"{str(plageMin)}-{str(plageMax)}nm" #str est facultatif mais c'est plus explicite
+    dico1[plage]=[]
+
+
+    #Détermination du nombre de décimal de tailleFenetre si celui-ci n'est pas entier:
+    nbDecimal=0
+    tailleFenetreEntier=tailleFenetre
+    while not tailleFenetreEntier.is_integer():
+        tailleFenetreEntier=tailleFenetreEntier*10**nbDecimal
+        nbDecimal+=1
+
+
+    fd=open(cheminFichier)
+
+    if fd != 0 :
+        
+        lignes = fd.readlines()
+        
+        for ligne in lignes :
+            
+            res=re.search("(^\d+\.\d+).(-?\d+\.\d+)",ligne)
+            #le ^ c'est pour le début, \d c'est pour un chiffre, le + c'est une répétition du terme d'avant, . c'est pour n'importe quel caractère, le ? c'est pour optionnel
+            
+            if res : #Permet de ne sélectionner que les lignes avec les données 
+                
+                longueur=float(res.group(1))
+                intensite=float(res.group(2))
+                
+
+                if plageMin <= longueur and longueur < plageMax : #On est dans la plage donc la clée est déjà créé
+                    dico1[plage].append(intensite)
+                
+
+                elif longueur >= plageMax :                                   #On sort de la plage mais on n'est pas forcément dans la plage suivante !
+                    while not(plageMin <= longueur and longueur < plageMax) : #Tant qu'on n'est pas dans la plage...
+                        plageMin = plageMax                                   #La plage min devient l'ancienne plage max
+                        if tailleFenetre.is_integer():                        #On rajoute le pas à la plage max et on nomme la nouvelle plage
+                            plageMax += int(tailleFenetre)                    #Entier pour l'encodage du dico 
+                            plage = f"{str(plageMin)}-{str(plageMax)}nm"                   
+                        else :
+                            plageMax += tailleFenetre
+                            plage = f"{str(round(plageMin,nbDecimal))}-{str(round(plageMax,nbDecimal))}nm" 
+                        dico1[plage]=[]                                       #On crée l'entrée du dico pour cette plage avec une valeur vide
+                    #A la sortie du while, la clée est créée pour la longueur actuelle
+                    dico1[plage].append(intensite)
+
+
+                else : # longueur < plageMin ce qui est normalement impossible
+                    print("ERREUR : longueur < plageMin à la ligne :")
+                    print(ligne)
+
+    else :
+        print("ERREUR : le fichier ne marche pas")
+        exit()
+    
+    return dico1
+
+
+def deuxiemeDico(dico1):
+    #Création du deuxième dictionnaire avec les statistiques
+
+    dico2={}
+
+    for plage in dico1:
+        
+        liste=dico1[plage]
+        liste.sort()
+        nombre=len(liste)
+        
+        if nombre != 0 :
+            minimum=liste[0]
+            moyenne=sum(liste)/nombre
+            maximum=liste[-1]
+
+        else : #Pour les listes vides ex la plage 0-10nm si tailleFenetre=10
+            minimum=None
+            moyenne=None
+            maximum=None
+            #J'ai choisi de mettre None quand il n'y a pas de donnée car 0 est en soi une intensité donc physiquement c'est faux
+
+        dico2[plage]=[nombre,minimum,moyenne,maximum]
+
+    return dico2
+
+help()
+
+#cheminFichier,tailleFenetre = help()
+#print(deuxiemeDico(premierDico(cheminFichier,tailleFenetre)))
---
+++ recherche_plot.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Auteur : Joanny Magat
+
+"""
+Script qui demande en input à l'utilisateur un intervalle de longueur d'onde
+(sachant que le fichier .txt et la taille de fenêtre de longueur d'onde est demandé en paramètre du script)
+et affiche le plot de l'intensité en fonction de la longueur d'onde sur l'intervalle demandé.
+"""
+
+#python3 recherche_plot.py /home/e20210012399/Documents/M1/SYSTEME/PROJET/Spectre_photoluminescence.txt
+
+import matplotlib.pyplot as plt 
+import intensite
+
+cheminFichier,tailleFenetre = intensite.help()
+dico=intensite.deuxiemeDico(intensite.premierDico(cheminFichier,tailleFenetre))
+
+
+def demandeIntervalle(tailleFenetre):
+    #Demande de l'intervalle respectant la taille de fenêtre de longueur d'ondes
+
+    print("Quel intervalle voulez-vous ? (Doit respecter la taille des fenêtres de longueur d'onde)")
+    intervallePasBon = True
+    while intervallePasBon :
+        
+        intervalleMin=float(input("Borne inférieur de l'intervalle :"))
+        intervalleMax=float(input("Borne supérieur de l'intervalle :"))
+
+        tailleFenetreEntier=tailleFenetre #Si la tailleFenetre n'est pas entière
+        iTailleFenetre=0 #Compteurs permettant de savoir combien de fois on a multiplié tailleFenetre par 10 jusqu'à avoir un entier
+
+        while not tailleFenetreEntier.is_integer():
+            tailleFenetreEntier=tailleFenetreEntier*10**iTailleFenetre
+            iTailleFenetre+=1
+
+        if intervalleMin*10**iTailleFenetre % int(tailleFenetreEntier) == 0 and intervalleMax*10**iTailleFenetre % int(tailleFenetreEntier) == 0 and 0<=intervalleMin<intervalleMax :
+            #On a testé si les bornes étaient multiples de tailleFenetre pour avoir les bonnes plages par la suite
+            intervallePasBon = False
+
+        else :
+            print("Bornes invalides par rapport à la taille des fenêtres de longueurs d'ondes")
+
+    # #Arrondi des intervalles pour qu'ils correspondent à la taille de fenêtres de longueur d'onde à la place ?
+
+    return intervalleMin,intervalleMax
+
+
+
+def abscisse(tailleFenetre,intervalleMin,intervalleMax):
+    x=[]
+    
+    #Détermination du nombre de décimal de tailleFenetre si celui-ci n'est pas entier :
+    nbDecimal=0
+    tailleFenetreEntier=tailleFenetre
+    while not tailleFenetreEntier.is_integer():
+        tailleFenetreEntier=tailleFenetreEntier*10**nbDecimal
+        nbDecimal+=1
+
+    #Détermination du nombre de valeur dans la liste des abscisses :
+    nbValeurs=(intervalleMax-intervalleMin)/tailleFenetre #Nombre de valeurs entre la borne inf et sup avec la pas choisis
+    if not nbValeurs.is_integer():
+        nbValeurs = 1 + int(nbValeurs) #Exemple : si intervalle [0,10.2] avec pas de 0.1 permet d'avoir [10.1-10.2]
+    
+    #Ajout des valeurs dans x :
+    for borneInf in [intervalleMin+n*tailleFenetre for n in range(0,int(nbValeurs))]:
+        #Puisque le pas peut être un float, on ne pouvait pas faire un simple range d'où la solution ci-dessus
+        if tailleFenetre.is_integer():
+            x.append(f"{int(borneInf)}-{int(borneInf+tailleFenetre)}")
+        else :
+            x.append(f"{round(borneInf,nbDecimal)}-{round(borneInf+tailleFenetre,nbDecimal)}")
+    return x
+
+
+def ordonnee(dico,abscisse):
+    y=[]
+    for plage in abscisse:
+        if f"{plage}nm" in dico:
+            y.append(dico[f"{plage}nm"][2]) #2 correpond à la moyenne
+        else :
+            y.append(None)
+    return y
+
+def incertitude(dico,abscisse):
+    y_inc=[]
+    for plage in abscisse:
+        if f"{plage}nm" in dico:
+            minimum=dico[f"{plage}nm"][1]
+            maximum=dico[f"{plage}nm"][3]
+            if minimum != None and maximum != None :
+                y_inc.append((maximum-minimum)/2**0.5)
+            else :
+                y_inc.append(None)
+        else :
+            y_inc.append(None)
+    return y_inc
+
+def suppressionNone(x,y,y_inc): #Pas parfait car supprime le nom de l'abscisse aussi
+    x_co=[]
+    y_co=[]
+    y_inc_co=[]
+    for i in range(len(x)):
+        if y[i] != None :
+            x_co.append(x[i])
+            y_co.append(y[i])
+            y_inc_co.append(y_inc[i])
+    return x_co,y_co,y_inc_co
+
+
+#Affichage du plot :
+
+intervalleMin,intervalleMax = demandeIntervalle(tailleFenetre)
+
+x=abscisse(tailleFenetre,intervalleMin,intervalleMax)
+y=ordonnee(dico,x)
+y_inc=incertitude(dico,x)
+
+x,y,y_inc=suppressionNone(x,y,y_inc)
+
+plt.title("Intensité moyenne en fonction des plages de longueurs d'onde en nm avec incertitude")
+plt.xlabel("Plage de longueurs d'ondes en nm")
+plt.ylabel("Intensité moyenne")
+
+#Gestion de la graduation de l'abscisse pour éviter les chevauchements
+positions = range(len(x))
+graduationMax=100 #Nombre de graduation arbitraire pour que les graduations ne se chevauchent pas 
+pasAbscisse = max(1,len(x)//graduationMax)
+plt.xticks(positions[::pasAbscisse],rotation=90) #Tourne les abscisses et affiche 1 abscisse sur log(len(x)) 
+
+plt.plot(x,y,'g-')
+plt.errorbar(x,y,yerr=y_inc,fmt='o',capsize=5,ecolor='red') #fmt=style de point, capsize=taille des barres horizontales au bout des incertitudes, ecolor=couleur des barres d'erreur 
+
+plt.tight_layout() #Ajuste automatiquement les marges
+plt.show()
