37ba15a02d8cc49d95aca155232727c9344fe0e9
Joanny-Magat <joanny.magat@etu.umontpellier.fr>
2025-12-09 08:45:51 +0100

    Test commentaire git 1

--- intensite.py
+++ intensite.py
@@ -9,7 +9,7 @@ Il renvoie un dictionnaire de liste dont la clée est la plages de longueurs d'o
 la valeur une liste contenant le nombre d'intensité par plage, le minimum d'intensité pour la plage,
 la moyenne et le maximum. 
 """
-#Test changement git
+#Test changement git 1
 #python3 intensite.py /home/e20210012399/Documents/M1/SYSTEME/PROJET/Spectre_photoluminescence.txt
 
 import sys, re
